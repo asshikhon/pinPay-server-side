3444a17530b601d7968560d908e03e5d0a41e50b
asshikhon <asshikhon@gmail.com>
2024-07-17 15:41:03 +0600

    create initial file with mongodb added and registetion api added

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./db/takaDB.js";
+import { corsOptions } from "./configs/corsConfig.js";
+import authRoutes from "./routes/authentication.js";
+import userRoutes from "./routes/users.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewares
+app.use(cors(corsOptions));
+app.use(express.json());
+
+// routes
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
+
+app.get("/", async (req, res) => {
+    res.send("Taka Server is Running!");
+});
+
+const run = async () => {
+    await connectDB();
+
+    app.listen(port, () => {
+        console.log(`Taka Server is Running on Port: ${port}`);
+    });
+};
+
+run().catch(console.dir);
